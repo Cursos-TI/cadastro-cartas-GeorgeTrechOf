5dd22399884a2cccf13dc79354f57314969fb196
GeorgeTrechOf <136534268+GeorgeTrechOf@users.noreply.github.com>
2026-03-24 17:45:47 +0000

    Primeira tentativa de termonar o desafil pra novatos

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -13,12 +13,25 @@ int main() {
   int Numero_de_pontos_turisticos;
 
   // Área para entrada de dados
-  printf("Qual o nome a populkação da cidade:");
-  scanf("%d",& populacao)
+  printf("Qual o nome a população da cidade ? \n");
+  scanf("%d",& populacao); 
+
+  printf("Qual a área da cidade ?\n");
+  scanf ("%f",& area);
+
+  printf("Qual o PIB da cidade ?\n");
+  scanf ("%f",& PIB);
+
+  printf("Quantos pontos turísticos ela tem ?");
+  scanf ("%d",& Numero_de_pontos_turisticos);
 
 
   // Área para exibição dos dados da cidade
-  printf("A população da cidade é %d" nome);
+  
+  printf ("A população da cidade é de %d\n", populacao);
+  printf ("A área é: %f\n", area);
+  printf ("O PIB é de: %f \n" PIB)
+  printf ("A quatidade de pontos turísticos é: %d" Numero_de_pontos_turisticos)
 
 return 0;
 } 
